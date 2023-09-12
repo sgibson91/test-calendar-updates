f5fd4a9a12b457fdf4e10b2250c3f4f83b0c396c
Sarah Gibson <drsarahlgibson@gmail.com>
2023-09-12 11:02:47 +0100

    print dict keys

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ if issue_context is None:
     raise ValueError("ISSUE_CONTEXT must be provided!")
 
 labels = [label["name"] for label in issue_context["labels"] if label["name"] == "Time Off"]
-print(labels, len(labels))
 
 if len(labels) == 0:
     print("Issue does not have the appropriate label. Exiting script.")
     sys.exit()
+
+print(issue_context.keys())
