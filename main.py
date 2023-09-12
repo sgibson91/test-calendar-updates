00bb1224c71540861a49e877489a5248497e6275
Sarah Gibson <drsarahlgibson@gmail.com>
2023-09-12 10:49:45 +0100

    Decide if label matches

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import os
 import json
+import os
+import sys
 
 issue_context = json.loads(os.environ.get("ISSUE_CONTEXT", None))
 if issue_context is None:
     raise ValueError("ISSUE_CONTEXT must be provided!")
 
-print(type(issue_context))
-print(issue_context)
+labels = [label["name"] for label in issue_context["labels"] if label["name"] == "Time Off"]
+print(labels, len(labels))
+
+if len(labels) == 0:
+    print("Issue does not have the appropriate label. Exiting script.")
+    sys.exit()
